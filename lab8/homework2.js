060927e455d0ff9e911098073bae45a589e5b1c6
sayal-aryal <sayalaryal2@gmail.com>
2023-05-10 21:35:00 -0500

    Lab 8 added

---
+++ lab8/homework2.js
@@ -0,0 +1,25 @@
+function Student(firstName, lastName, grade) {
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grade = grade;
+}
+
+let student1 = new Student("sayal", "Aryal", [55, 56, 57, 58])
+let student2 = new Student("John", "Doe", [56, 57, 58, 59, 60])
+
+Student.prototype.inputNewGrade = function (newGrade) {
+    this.grade.push(newGrade)
+}
+
+Student.prototype.computeAverageGrade = function () {
+    return this.grade.reduce((intial, grad) => intial + grad, 0) / this.grade.length
+}
+
+let studentList = [student1, student2]
+
+studentList[0].inputNewGrade(5)
+studentList[1].inputNewGrade(10)
+
+console.log(studentList[0].computeAverageGrade())
+console.log(studentList[1].computeAverageGrade())
